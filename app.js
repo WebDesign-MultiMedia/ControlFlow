be1bb6e94814e4388894b8ef53519b63ba277f59
j.salas2016 <jsalas198016@gmail.com>
2024-05-09 16:43:53 -0400

    Part One Done

--- app.js
+++ app.js
@@ -1,29 +1,78 @@
-const radius = 5;
+// area = 78.537500 Space available
 const PI = 3.1415;
+const radius = 5;
 const area = PI * radius * radius;
-const squareMeters= 0.8;
+
+// Each plant requires a minimum space 
+const minimunSpace= 0.8;
+
+//Starting plants
+const plants = 20;
+
+// every week plants double
 const weekOne = 20;
 const weekTwo = 40;
 const weekThree = 80;
 
-  //Plant Area
-const weekOneArea = squareMeters * weekOne;
-console.log(weekOneArea);
+// PART ONE
 
-const weekTwoArea = squareMeters * weekTwo;
-console.log(weekTwoArea);
+function predictPlantGroth(weeks){
+  let plantAmount = plants * (weeks * 2);
 
-const weekThreeArea = squareMeters * weekThree;
-console.log(weekThreeArea)
+   if (plantAmount > 0.8 * area){
+    return "Prune the plants.";
+   } 
+   else if (plantAmount > 0.5 * area) 
+   {
+    return "Monitor the growth.";
+   }
+   else {
+    return "Plant more plants."
+   }
+}
 
+// Percentage
+ const weekOnePercentage = (weekOne / area) * 100;
+ const weekTwoPercentage = (weekTwo / area) * 100;
+ const weekThreePercentage = (weekThree / area) * 100;
+
+
+console.log("week 1:", predictPlantGroth(1), weekOnePercentage);
+console.log("week 2:", predictPlantGroth(2), weekTwoPercentage);
+console.log("week 3:", predictPlantGroth(3), weekThreePercentage);
 
 // Percentage of total space
 
-const weekOnePercentage = (weekOneArea / area) * 100;
-console.log(weekOnePercentage);
 
-const weekTwoPercentage = (weekTwoArea / area) * 100;
-console.log(weekTwoPercentage);
 
-const weekThreePercentage = (weekThreeArea / area) * 100;
-console.log(weekThreePercentage);
+
+
+
+
+
+
+
+
+
+
+
+
+//Plants double every week
+//const weekOne = 20;
+//const weekTwo = 40;
+//const weekThree = 80;
+ 
+
+
+  //Plant Area
+//const weekOneArea = minimunSpace * weekOne;
+//console.log(weekOneArea);
+
+//const weekTwoArea = minimunSpace * weekTwo;
+//console.log(weekTwoArea);
+
+//const weekThreeArea = minimunSpace * weekThree;
+//console.log(weekThreeArea)
+
+
+
