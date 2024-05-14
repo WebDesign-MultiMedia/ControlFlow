6e5680a5d561a922936ee9fc8a8791fb13efc30f
j.salas2016 <jsalas198016@gmail.com>
2024-05-13 21:14:34 -0400

    All done, Thanks to you Steve. I still need to practice more.

--- app.js
+++ app.js
@@ -1,78 +1,25 @@
-// area = 78.537500 Space available
 const PI = 3.1415;
 const radius = 5;
 const area = PI * radius * radius;
+const intitalPlants = 20;
+const spacePerPlant = 0.8;
+const maxCapacity = area / spacePerPlant;
+// PC means plantCount
+const eightyPercent = (PC) => PC > maxCapacity * spacePerPlant;
+const fifthyToEighty = (PC) => PC => maxCapacity * 0.8;
 
-// Each plant requires a minimum space 
-const minimunSpace= 0.8;
+function plantGrowthControl(weeks){
+ const plantCount = intitalPlants * Math.pow(2, weeks);
 
-//Starting plants
-const plants = 20;
-
-// every week plants double
-const weekOne = 20;
-const weekTwo = 40;
-const weekThree = 80;
-
-// PART ONE
-
-function predictPlantGroth(weeks){
-  let plantAmount = plants * (weeks * 2);
-
-   if (plantAmount > 0.8 * area){
-    return "Prune the plants.";
-   } 
-   else if (plantAmount > 0.5 * area) 
-   {
-    return "Monitor the growth.";
-   }
-   else {
-    return "Plant more plants."
-   }
+ if(eightyPercent(plantCount)){
+  console.log(`week ${weeks}: Prune the plants. Too many plants!`);
+ } else if(fifthyToEighty(plantCount)) {
+  console.log(`week ${weeks}: Monitor the plants. They are growing within acceptable limits`);
+ } else{
+  console.log("week ${weeks}: Plant more. There is room for more plants." );
+ }
 }
 
-// Percentage
- const weekOnePercentage = (weekOne / area) * 100;
- const weekTwoPercentage = (weekTwo / area) * 100;
- const weekThreePercentage = (weekThree / area) * 100;
-
-
-console.log("week 1:", predictPlantGroth(1), weekOnePercentage);
-console.log("week 2:", predictPlantGroth(2), weekTwoPercentage);
-console.log("week 3:", predictPlantGroth(3), weekThreePercentage);
-
-// Percentage of total space
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Plants double every week
-//const weekOne = 20;
-//const weekTwo = 40;
-//const weekThree = 80;
- 
-
-
-  //Plant Area
-//const weekOneArea = minimunSpace * weekOne;
-//console.log(weekOneArea);
-
-//const weekTwoArea = minimunSpace * weekTwo;
-//console.log(weekTwoArea);
-
-//const weekThreeArea = minimunSpace * weekThree;
-//console.log(weekThreeArea)
-
-
-
+plantGrowthControl(1);
+plantGrowthControl(2);
+plantGrowthControl(3);
